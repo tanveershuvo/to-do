eda59a6efecfcb64839ab4a95a073457c94bebf0
tanveershuvo <tanveershuvos@gmail.com>
2020-09-21 22:32:26 +0600

    HTTPS forcing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (config('app.env') === 'production') \URL::forceScheme('https');
+
     }
 
     /**
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
